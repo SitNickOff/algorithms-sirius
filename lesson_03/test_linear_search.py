c27288f69cbf834de1180f845f2b115b14c6e9eb
Igor <89976250+Dubrovsky18@users.noreply.github.com>
2021-09-20 01:07:16 +0300

    Update and rename test_search.py to test_linear_search.py

---
+++ lesson_03/test_linear_search.py
@@ -0,0 +1,7 @@
+import unittest
+from linear_search import linear_search
+
+
+class TestCircleArea(unittest.TestCase):
+    def test_primary(self):
+        self.assertEqual(linear_search([0,1,7,19,17,20,10],20),5)
