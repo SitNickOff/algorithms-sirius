f0dd0855f104a0872c9425641e25c0109831e4c4
Dubrovsky18 <dubrovin.02@list.ru>
2021-09-22 09:04:52 +0300

    поставьте 5 пожалуйста

---
+++ group101/Dubrovin_Igor/task_04/test_Simple_number.py
@@ -0,0 +1,8 @@
+import unittest
+from Simple_numbers import Simple_numbers
+
+
+class TestCircleArea(unittest.TestCase):
+    def test_primary(self):
+        self.assertEqual(Simple_numbers((6), "-1"))
+        
