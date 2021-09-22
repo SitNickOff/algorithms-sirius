2d8d2a3ec56cde3223f4c73fd7b30bd2b3079052
JuliaJX <JuliaJX28@gmail.com>
2021-09-22 08:35:22 +0300

    .

--- group101/VinogradovaYulia/task03/linear_search.py
+++ group101/VinogradovaYulia/task03/linear_search.py
@@ -1,15 +1,13 @@
-def linear_search(numbers, x):
-    for index in range(len(numbers)):
-        if numbers[index] == x:
-            return index
+# def linear_search(numbers, x):
+#     for index in range(len(numbers)):
+#         if numbers[index] == x:
+#             return index
 
-    return -1
+#     return -1
 
-def binary_search(numbers,x):
-    return -1
+# def binary_search(numbers,x):
+#     return -1
     
-
-
 n = 2
 numbers = [10,3,25,38,14,1,28,45]
 x=28
@@ -22,3 +20,13 @@ if right_result == result1:
     print("Число линейное", result1, right_result)
 else:
     print("Число бинарное", result2, right_result)
+
+def linear_search(numbers, x):
+    for index in range(len(numbers)):
+        if numbers[index] == x:
+            return index
+
+    return -1
+
+def binary_search(numbers,x):
+    return -1
