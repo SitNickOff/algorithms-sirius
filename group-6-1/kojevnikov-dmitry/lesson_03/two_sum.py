99e6ae69c6dad427c64c1e88c3edb38b20f7a5b9
Andrey Sitnikov <33022803+SitNickOff@users.noreply.github.com>
2021-11-07 15:51:46 +0300

    add group-6-1-kojevnikov-dmitry branch

---
+++ group-6-1/kojevnikov-dmitry/lesson_03/two_sum.py
@@ -0,0 +1,20 @@
+# Дан массив целых чисел numbers и целое число X; 
+# нужно найти в массиве два элемента, сумма которых равняется X. 
+# Гарантируется, что такие элементы в массиве есть.
+
+# 1. Решить задачу применяя наивный алгоритм
+# 2. Предлжить второе решение, которое оптимизрует наивны алгоритм 
+# с применеием сортировки или вспомогательных структур
+
+def two_sum_primary(numbers, sum):
+    return [1, 9]
+
+def two_sum_optimized(numbers, sum):
+    return [3, 5]
+
+# numbers = list(map(int, input().split()))
+# sum = int(input())
+
+# print('primary', two_sum_primary(numbers, sum))
+# print('optimized', two_sum_primary(numbers, sum))
+
