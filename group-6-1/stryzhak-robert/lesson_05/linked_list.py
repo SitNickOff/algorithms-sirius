fc631a27681134969994286e1b5a2c76195f8faf
Bob4inski <robertstryzhak@gmail.com>
2021-11-10 18:53:57 +0300

    save classwork

--- group-6-1/stryzhak-robert/lesson_05/linked_list.py
+++ group-6-1/stryzhak-robert/lesson_05/linked_list.py
@@ -9,23 +9,23 @@ def print_linked_list(vertex):
                 vertex = vertex.next
         print("None") 
 
-# def get_node_by_index(node, index):
-#         while index:
-#             node = node.next
-#             index -= 1
-#         return node
+def get_node_by_index(node, index):
+        while index:
+                node = node.next
+                index -= 1
+        return node
 
-# def insert_node(head, index, value):
-#         new_node = Node(value)
+def insert_node(head, index, value):
+        new_node = Node(value)
 
-#         if index == 0:
-#             new_node.next = head
-#             return new_node
+        if index == 0:
+                new_node.next = head
+                return new_node
 
-#         previous_node = get_node_by_index(head, index-1)
-#         new_node.next = previous_node.next 
-#         previous_node.next = new_node
-#         return head
+        previous_node = get_node_by_index(head, index-1)
+        new_node.next = previous_node.next 
+        previous_node.next = new_node
+        return head
 
 
 node3 = Node('Node3')
