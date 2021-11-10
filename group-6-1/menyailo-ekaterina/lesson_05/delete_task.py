b0a366842a52b8abc6f20e66557924ade6c304a1
Katya Menyaylo <katya/menyaylo/04@gmail.com>
2021-11-10 22:17:26 +0300

    hard tasks5

--- group-6-1/menyailo-ekaterina/lesson_05/delete_task.py
+++ group-6-1/menyailo-ekaterina/lesson_05/delete_task.py
@@ -25,7 +25,13 @@ def print_linked_list(vertex):
 
 # Необходимо заменить pass на Ваш код
 def solution(node, idx):
-    pass
+    if node == None:
+        return node
+    if idx == 0:
+        return node.next_item
+    prev_node = get_node_by_index(node,idx - 1)
+    prev_node.next_item = prev_node.next_item.next_item
+    return node
 
 def test():
     task5 = Node("Реализовать операцию вывода на печать текущие задания в обратном порядке", None)
@@ -37,6 +43,7 @@ def test():
     new_head = solution(task0, 5)
     new_head = solution(new_head, 2)
     new_head = solution(new_head, 0)
+
     # result is Реализовать связанный список -> Реализовать операцию добавления элемента в список -> Реализовать операцию удаления элемента из списка -> None
     print_linked_list(new_head)
 
