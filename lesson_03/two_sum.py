ec3fafa94409c91034bf5dddd6119d2747a73178
NotjustIvan <notjustivan@gmail.com>
2021-11-11 10:39:51 +0300

    add readme, linear, binary and some two_sum

--- lesson_03/two_sum.py
+++ lesson_03/two_sum.py
@@ -3,18 +3,31 @@
 # Гарантируется, что такие элементы в массиве есть.
 
 # 1. Решить задачу применяя наивный алгоритм
-# 2. Предлжить второе решение, которое оптимизрует наивны алгоритм 
+# 2. Предложить второе решение, которое оптимизрует наивный алгоритм 
 # с применеием сортировки или вспомогательных структур
 
 def two_sum_primary(numbers, sum):
-    return [1, 9]
+    for i in numbers:
+        h_list = numbers.remove(i)
+        x = sum - i
+        for ii in h_list:
+            if ii == x:
+                stroka = str(i) + ' and ' + str(ii)
+                return stroka
 
 def two_sum_optimized(numbers, sum):
+    for i in numbers:
+        h_list = numbers.remove(i)
+        x = sum - i
+        for ii in h_list:
+            if ii == x:
+                stroka = str(i) + ' and ' + str(ii)
+                return stroka
     return [3, 5]
 
-# numbers = list(map(int, input().split()))
-# sum = int(input())
+numbers = list(map(int, input().split()))
+sum = int(input())
 
-# print('primary', two_sum_primary(numbers, sum))
-# print('optimized', two_sum_primary(numbers, sum))
+print('primary', two_sum_primary(numbers, sum))
+#print('optimized', two_sum_optimized(numbers, sum))
 
