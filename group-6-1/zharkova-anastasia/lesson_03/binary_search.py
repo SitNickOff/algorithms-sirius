d115228c7836ed66913d180df4cad539145aa241
Anastasia Zharkova <nastya.zharkova.03@bk.ru>
2021-11-14 12:26:00 +0300

    added classwork from lesson-03

--- group-6-1/zharkova-anastasia/lesson_03/binary_search.py
+++ group-6-1/zharkova-anastasia/lesson_03/binary_search.py
@@ -2,20 +2,20 @@
 # вернуть его индекс. Если x в массиве не встречается - вернуть -1. 
 
 # Решить задачу применяя алгоритм бинарного поиска
-def sort(n): # чтобы искать число в списке, сначала надо его отсортировать для бинарного поиска
+def sort(n): 
     for y in range(len(n)):
         for i in range(len(n)-y-1):
             if n[i] > n[i+1]:
                 n[i], n[i+1] = n[i+1], n[i]
-    return n #возвращаем упорядочный массив
+    return n 
 
 def binary_search(numbers, x):
-    sor = sort(numbers)     #сортируем список
-    lent = len(sor)-1     #будем идти от конца
-    mid = len(sor) //2     # середина, которая ищит индекс
-    zero = 0              #будем идти от начала
-    print("отсортированный список: ", sor)
-    while sor[mid] != x and zero <=lent:  # пока два условия выполняются цикл будет работать
+    sor = sort(numbers)    
+    lent = len(sor)-1     
+    mid = len(sor) //2     
+    zero = 0             
+    print("Отсортированный список: ", sor)
+    while sor[mid] != x and zero <=lent:  
         if x > sor[mid]:
             zero = mid +1
         else:
@@ -26,12 +26,12 @@ def binary_search(numbers, x):
     else:
         return mid
 
-list_num = list(map(int, input("задайти список: ").split())) #задаем список чисел
-x = int(input('введите число, индекс которого вы ищите')) #пользователь задает число, которое нам надо найти
+list_num = list(map(int, input("Задайте список: ").split())) 
+x = int(input('Введите число, индекс которого нужно найти')) 
 poznany = binary_search(list_num, x)
 if poznany == -1:
     print("Всё работает, такого числа нет,", poznany)
 elif poznany != -1:
     print("Всё работает, такое число есть. Число", x ,"под индексом", poznany)
 else:
-    print("НИЧЕГО не работает. ---ОШИБКА---")
+    print("Ничего не работает, ошибка")
--- group-6-1/zharkova-anastasia/lesson_03/linear_search.py
+++ group-6-1/zharkova-anastasia/lesson_03/linear_search.py
@@ -4,17 +4,17 @@
 # Решить задачу применяя алгоритм бинарного поиска
 
 def linear_search(numbers, x):
-    for index in range(len(numbers)):   #так как это линейный поиск, будем перебирать все числа в списке
+    for index in range(len(numbers)):  
         if numbers[index] == x:
-            return index       #если нашлось число возвращаем индекс найденного числа
-    return -1                   #если ничего не нашлось, возвращаем -1
+            return index       
+    return -1                   
 
-list_num= list(map(int, input("запишите числа, среди которых мы будем искать").split())) #задаем массив
-num = int(input("Введите число индекс, которого вы ищите: ")) #число которое, мы будем искать
+list_num= list(map(int, input("Запишите числа, среди которых нужно искать").split())) 
+num = int(input("Введите число-индекс, которое нужно найти: ")) 
 poznany = linear_search(list_num,num)
 if poznany == -1:
     print("Всё работает, такого числа нет,", poznany)
 elif poznany != -1:
     print("Всё работает, такое число есть. Число", num ,"под индексом", poznany)
 else:
-    print("НИЧЕГО не работает. ---ОШИБКА---")
+    print("Ничего не работает, ошибка")
