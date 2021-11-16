57edb5aea773ef2f191ed6b327500d04a2bea908
Arseny <irusap54@gmail.com>
2021-11-16 23:49:44 +0300

    download all

--- group106/BorovoyArseny/lesson_03/linear_search.py
+++ group106/BorovoyArseny/lesson_03/linear_search.py
@@ -4,24 +4,4 @@
 # Решить задачу применяя алгоритм бинарного поиска
 
 def linear_search(numbers, x):
-    for i in range(len(numbers)):
-        if numbers[i] == x:
-            return i
-    else:
-        return -1
-
-def test(res, aw_res):
-    if res == aw_res:
-        print('ok')
-    else:
-        print('not ok')
-
-a = [1, 3, 4, 5, 7, 8, 10]
-print('введите элемент для поиска в списке')
-n = int(input())
-print(linear_search(a, n))
-
-test(linear_search(a,1), 0)
-test(linear_search(a,2), -1)
-test(linear_search(a,5), 3)
-test(linear_search(a,15), -1)
+    return -1
--- group106/BorovoyArseny/lesson_03/two_sum.py
+++ group106/BorovoyArseny/lesson_03/two_sum.py
@@ -6,29 +6,15 @@
 # 2. Предлжить второе решение, которое оптимизрует наивны алгоритм 
 # с применеием сортировки или вспомогательных структур
 
-from math import pi
-import unittest
-
 def two_sum_primary(numbers, sum):
-    for i in range(len(numbers)):                       
-        for j in range(len(numbers)):                   
-            if j != i:                                  
-                if numbers[i] + numbers[j] == sum:      
-                    return [numbers[i], numbers[j]]
-    return -1
+    return [1, 9]
 
 def two_sum_optimized(numbers, sum):
-    for i in range(len(numbers)):
-        if (sum - numbers[i]) in numbers:
-            return [numbers[i], sum - numbers[i]]                                      
-    return -1                                       
+    return [3, 5]
 
-def test(res, aw_res):
-    if res == aw_res:
-        print("it's ok")
-    else:
-        print('not ok')
+# numbers = list(map(int, input().split()))
+# sum = int(input())
 
-test(two_sum_primary([1, 2, 3, 5, 9], 10), [1, 9])
-test(two_sum_optimized([1, 2, 3, 5, 9], 8), [3, 5])
+# print('primary', two_sum_primary(numbers, sum))
+# print('optimized', two_sum_primary(numbers, sum))
 
