08bac98cab8bb79cb09ed8e0cb6a455c19b49412
Arseny <irusap54@gmail.com>
2021-11-17 08:34:39 +0300

    net

--- group106/BorovoyArseny/lesson_03/two_sum.py
+++ group106/BorovoyArseny/lesson_03/two_sum.py
@@ -10,17 +10,9 @@ from math import pi
 import unittest
 
 def two_sum_primary(numbers, sum):
-    for i in range(len(numbers)):                       
-        for j in range(len(numbers)):                   
-            if j != i:                                  
-                if numbers[i] + numbers[j] == sum:      
-                    return [numbers[i], numbers[j]]
     return -1
 
 def two_sum_optimized(numbers, sum):
-    for i in range(len(numbers)):
-        if (sum - numbers[i]) in numbers:
-            return [numbers[i], sum - numbers[i]]                                      
     return -1                                       
 
 def test(res, aw_res):
@@ -29,5 +21,5 @@ def test(res, aw_res):
     else:
         print('not ok')
 
-test(two_sum_primary([1, 2, 3, 5, 9], 10), [1, 9])
-test(two_sum_optimized([1, 2, 3, 5, 9], 8), [3, 5])
+# test(two_sum_primary([1, 2, 3, 5, 9], 10), [1, 9])
+# test(two_sum_optimized([1, 2, 3, 5, 9], 8), [3, 5])
