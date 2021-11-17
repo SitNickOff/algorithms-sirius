7670480a5bd1c4529763a833420a5f2e7cdcce21
urcop <kirillzama@list.ru>
2021-11-17 10:16:31 +0300

    2-4 lesson

--- group-6-1/zagrebin-kirill/lesson_03/binary_search.py
+++ group-6-1/zagrebin-kirill/lesson_03/binary_search.py
@@ -3,5 +3,23 @@
 
 # Решить задачу применяя алгоритм бинарного поиска
 
-def binary_search(numbers, x):
-    return -1
+def binary_search(array, num):
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        mid = low + high
+        guess = array[mid]
+
+        if guess == num:
+            return mid
+        if guess > mid:
+            high = mid - 1
+        if guess < mid:
+            low = mid + 1
+
+    return None
+
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4))
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1))
--- group-6-1/zagrebin-kirill/lesson_04/extra_letter.py
+++ group-6-1/zagrebin-kirill/lesson_04/extra_letter.py
@@ -9,8 +9,14 @@
 # Выведите лишнюю букву.
 
 def extra_letter(s, t):
-
-    return 'e'
+    if len(s) > len(t):
+        for i in s:
+            if s.count(i) != t.count(i):
+                return i
+    else:
+        for i in t:
+            if t.count(i) != s.count(i):
+                return i
 
 
 def test(s, t, result):
@@ -18,6 +24,7 @@ def test(s, t, result):
         print('Ошибка! Ожидали: ', result, ' -  Получили: ', extra_letter(s, t))
     else:
         print('Отлично: ', result, '==', extra_letter(s, t))
+
 
 test('abcd', 'abcde', 'e')
 test('go', 'ogg', 'g')
