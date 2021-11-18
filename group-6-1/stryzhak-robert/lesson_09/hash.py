973193d94771abf9ee85e421fdcfa924322d0d32
Bob4inski <robertstryzhak@gmail.com>
2021-11-18 13:50:02 +0300

    классная раьота 9 урок

--- group-6-1/stryzhak-robert/lesson_09/hash.py
+++ group-6-1/stryzhak-robert/lesson_09/hash.py
@@ -1,6 +1,30 @@
-def hash(string):
-    dlin = len(string)
-    string_new = string[0:dlin-1]
-    if dlin == 1:
-        return ord(string[0])*(dlin-1)
-    return (hash(string_new)*(dlin-1) + 
+class Map():
+    def __init__(self):
+        self.pairs = []
+        
+    def set(self, key, value):
+        self.pairs.append([key, value])
+    
+    def get(self, key):
+        for item in self.pairs:
+            if item[0] == key:
+                return item[1]
+        return -1
+
+    
+def test():
+    cities1 = Map()
+    cities1.set('city1', 'Moscow')    
+    cities1.set('city2', 'Tula')    
+    cities1.set('city3', 'LA')
+    
+
+    print(cities1.pairs)
+    assert cities1.get('city1') == 'Moscow'
+    assert cities1.get('city3') == 'LA'
+    
+    print(cities1.get('city1'))    
+    print(cities1.get('city2'))
+
+    
+test()
