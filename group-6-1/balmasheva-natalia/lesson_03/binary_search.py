2e081f54a13055d28e4ebeaadfe21dd30ed8ab77
balmashevani <tata0372@yandex.ru>
2021-11-21 17:45:32 +0300

    add lesson03

--- group-6-1/balmasheva-natalia/lesson_03/binary_search.py
+++ group-6-1/balmasheva-natalia/lesson_03/binary_search.py
@@ -4,4 +4,28 @@
 # Решить задачу применяя алгоритм бинарного поиска
 
 def binary_search(numbers, x):
-    return -1
+    sor = sort(numbers)     #сортируем список
+    lent = len(sor)-1     #будем идти от конца
+    mid = len(sor) //2     # середина, которая ищит индекс
+    zero = 0              #будем идти от начала
+    print("отсортированный список: ", sor)
+    while sor[mid] != x and zero <=lent:  # пока два условия выполняются цикл будет работать
+        if x > sor[mid]:
+            zero = mid +1
+        else:
+            lent = mid -1
+        mid = (zero+lent)//2
+    if zero > lent:
+        return -1
+    else:
+        return mid
+
+list_num = list(map(int, input("задайти список: ").split())) #задаем список чисел
+x = int(input('введите число, индекс которого вы ищите')) #пользователь задает число, которое нам надо найти
+poznany = binary_search(list_num, x)
+if poznany == -1:
+    print("Всё работает, такого числа нет,", poznany)
+elif poznany != -1:
+    print("Всё работает, такое число есть. Число", x ,"под индексом", poznany)
+else:
+    print("НИЧЕГО не работает. ---ОШИБКА---")
