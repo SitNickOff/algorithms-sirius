aa92e537091fbce66f82ca40f872bab97470c478
Dubrovsky18 <dubrovin.02@list.ru>
2021-12-01 23:21:26 +0300

    Lesson_4

--- lesson_04/factorization.py
+++ lesson_04/factorization.py
@@ -13,8 +13,17 @@
 # Выведите в порядке неубывания простые множители, на которые раскладывается число n.
 
 def factorizate(n):
-    
-    return [n]
+    i = 2
+    num = []
+    while (n != 1) or (n == i):
+        if n % i == 0:
+            n = n / i
+            num.append(i)
+            i = 2
+            print(num)
+        else:
+            i += 1
+    return num
 
 
 def test(n, result):
