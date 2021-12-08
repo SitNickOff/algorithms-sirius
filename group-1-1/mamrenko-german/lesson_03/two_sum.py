10736cbf918c7308156c43f8a7a6cef51b6c67b7
meligera@ya.ru <meligera@ya.ru>
2021-12-08 22:28:36 +0300

    added 3rd lesson

--- group-1-1/mamrenko-german/lesson_03/two_sum.py
+++ group-1-1/mamrenko-german/lesson_03/two_sum.py
@@ -6,15 +6,31 @@
 # 2. Предлжить второе решение, которое оптимизрует наивны алгоритм 
 # с применеием сортировки или вспомогательных структур
 
+num_arr = [3, 5, 2, -4, 8, 11]
+pair_sum = 8
+
 def two_sum_primary(numbers, sum):
-    return [1, 9]
+    n = len(numbers)
+    for i in range(n):
+        for j in range(n):
+            if numbers[i] + numbers[j] == sum and i != j:
+                return [numbers[i], numbers[j]]
 
 def two_sum_optimized(numbers, sum):
-    return [3, 5]
+    hashTable = {}
+    for i in range(len(num_arr)):
+        complement = pair_sum - num_arr[i]
+        if complement in hashTable:
+            print("Pair with sum", pair_sum, "is: (", num_arr[i], ",", complement, ")")
+        hashTable[num_arr[i]] = num_arr[i]
 
-# numbers = list(map(int, input().split()))
-# sum = int(input())
+def test_primary(expected, result):
+    if expected != result:
+        print("Try again!")
+    else:
+        print("Seems OK!")
 
-# print('primary', two_sum_primary(numbers, sum))
-# print('optimized', two_sum_primary(numbers, sum))
+print(two_sum_primary(num_arr, pair_sum))
+test_primary(two_sum_primary(num_arr, pair_sum), [3, 5])
+print(two_sum_optimized(num_arr, pair_sum))
 
