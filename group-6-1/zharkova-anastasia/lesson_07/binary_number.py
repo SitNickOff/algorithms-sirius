9a5ad0c88e087306f768f276f07c1dd7573f54a0
Anastasia Zharkova <nastya.zharkova.03@bk.ru>
2021-12-10 16:17:33 +0300

    added tasks from lesson07

--- group-6-1/zharkova-anastasia/lesson_07/binary_number.py
+++ group-6-1/zharkova-anastasia/lesson_07/binary_number.py
@@ -1,11 +1,11 @@
-def binary_string(n):
+def binary_number(n):
     if n < 0:
-        return "-" + binary_string(-n)
+        return "-" + binary_number(-n)
 
     if n == 0 or n == 1:
         return str(n)
 
     last_digit = n % 2
-    return binary_string(n // 2) + str(last_digit)
+    return binary_number(n // 2) + str(last_digit)
 
-print(binary_string(251))
+print(binary_number(251))
