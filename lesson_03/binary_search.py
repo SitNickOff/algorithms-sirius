f80c4dfd9704bebe96543ad959fc54346ecf0fc0
Andrey Sitnikov <33022803+SitNickOff@users.noreply.github.com>
2021-12-12 22:11:41 +0300

    Revert "Lesson 03 complete"

--- lesson_03/binary_search.py
+++ lesson_03/binary_search.py
@@ -1,24 +1,7 @@
-# Дан массив целых чисел длины N. Нужно найти в нем заданное число x и
-# вернуть его индекс. Если x в массиве не встречается - вернуть -1.
+# Дан массив целых чисел длины N. Нужно найти в нем заданное число x и 
+# вернуть его индекс. Если x в массиве не встречается - вернуть -1. 
 
 # Решить задачу применяя алгоритм бинарного поиска
 
-def binary_search(mass, item):
-  low = 0
-  high = len(mass) - 1
-  i = 0
-  while low <= high:
-    mid = (low + high) // 2
-    guess = mass[mid]
-    if guess == item:
-      return mid
-    if guess > item:
-      high = mid - 1
-    else:
-      low = mid + 1
-    i += 1
-  return -1
-
-m = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(binary_search(m, 3))
-print(binary_search(m, 86))
+def binary_search(numbers, x):
+    return -1
