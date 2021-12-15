b2338431b886e02f879c88d464803aa3b2d2e811
Dmitry-Koz <cojevnikov.dmitry2017@gmail.com>
2021-12-15 09:25:58 +0300

    lesson-04

--- group-6-1/kojevnikov-dmitry/lesson_04/binary_system.py
+++ group-6-1/kojevnikov-dmitry/lesson_04/binary_system.py
@@ -29,8 +29,25 @@
 # 10
 
 def add(b1, b2):
-    binary = '0'
-
+    b22 = b2[::-1]
+    binary = ''
+    j = 0
+    sum = 0
+    for i, value in enumerate(b1[::-1], 0):
+        sum = int(value) + int(b22[i]) + j
+        if sum < 2:
+            binary = str(sum) + binary
+            j = 0
+        elif j > 0:
+            binary = '1' + binary
+            j -=1
+        elif j == 0:
+            binary = '0' + binary
+            j +=1
+    while j != 0:
+         binary = '1' + binary
+         j -= 1
+    
     return binary
 
 def test(b1, b2, result):
