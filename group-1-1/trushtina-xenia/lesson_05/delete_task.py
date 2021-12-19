291c7a4ac7af30f5e40e523dee68d5d1ce0fbb7e
aulunni <ksutrusha@yandex.ru>
2021-12-19 16:53:58 +0300

    lesson 05 is added

--- group-1-1/trushtina-xenia/lesson_05/delete_task.py
+++ group-1-1/trushtina-xenia/lesson_05/delete_task.py
@@ -23,21 +23,6 @@ def print_linked_list(vertex):
         vertex = vertex.next_item
     print("None") 
 
-# Необходимо заменить pass на Ваш код
-def solution(node, idx):
-    pass
-
-def test():
-    task5 = Node("Реализовать операцию вывода на печать текущие задания в обратном порядке", None)
-    task4 = Node("Реализовать операцию удаления элемента из списка", task5)
-    task3 = Node("Реализовать операцию добавления элемента в список", task4)
-    task2 = Node("Реализовать функцию, которая выведет на печать в терминале список текущих заданий", task3)
-    task1 = Node("Реализовать связанный список", task2)
-    task0 = Node("Проверить занимаемый данными объем оперативной памяти", task1)
-    new_head = solution(task0, 5)
-    new_head = solution(new_head, 2)
-    new_head = solution(new_head, 0)
-    # result is Реализовать связанный список -> Реализовать операцию добавления элемента в список -> Реализовать операцию удаления элемента из списка -> None
-    print_linked_list(new_head)
-
-test()
+def solution(node, self):
+    node.val = node.next.val
+    node.next = node.next.next
--- group-1-1/trushtina-xenia/lesson_05/reverse_list.py
+++ group-1-1/trushtina-xenia/lesson_05/reverse_list.py
@@ -1,16 +1,3 @@
-# Напишите функцию, которая вернёт список в обратном порядке.
-
-# Внимание: в этой задаче не нужно считывать входные данные. 
-# Нужно написать только функцию, которая принимает на вход 
-# голову двусвязного списка и возвращает голову перевёрнутого 
-# списка. 
-
-# Формат ввода
-# Функция принимает на вход единственный аргумент — голову двусвязного списка.
-
-# Формат вывода
-# Функция должна вернуть голову развернутого списка.
-
 class DoubleConnectedNode:  
     def __init__(self, value, next = None, prev = None):  
         self.value = value  
@@ -23,42 +10,14 @@ def print_linked_list(vertex):
         vertex = vertex.next
     print("None")
 
-# Необходимо заменить pass на Ваш код
-def solution(node):
-    pass
+def solution(self, head):
+    prev = None
+    cur = head
 
-def test():
-    task5 = DoubleConnectedNode("Реализовать операцию вывода на печать текущие задания в обратном порядке")
-    task4 = DoubleConnectedNode("Реализовать операцию удаления элемента из списка")
-    task3 = DoubleConnectedNode("Реализовать операцию добавления элемента в список")
-    task2 = DoubleConnectedNode("Реализовать функцию, которая выведет на печать в терминале список текущих заданий")
-    task1 = DoubleConnectedNode("Реализовать связанный список")
-    task0 = DoubleConnectedNode("Проверить занимаемый данными объем оперативной памяти")
-
-    task0.next = task1
-
-    task1.next = task2
-    task1.prev = task0
-
-    task2.next = task3
-    task2.prev = task1
-
-    task3.next = task4
-    task3.prev = task2
+    while cur:
+        n = cur.next
+        cur.next = prev
+        prev = cur
+        cur = n
     
-    task4.next = task5
-    task4.prev = task3
-
-    task5.prev = task4
-
-    new_head = solution(task0)
-    print_linked_list(new_head)
-
-    # result: 
-    # Реализовать операцию вывода на печать текущие задания в обратном порядке -> Реализовать 
-    # операцию удаления элемента из списка -> Реализовать операцию добавления элемента в 
-    # список -> Реализовать функцию, которая выведет на печать в терминале список текущих 
-    # заданий -> Реализовать связанный список -> Проверить занимаемый данными объем 
-    # оперативной памяти -> None
-
-test()
+    return prev
