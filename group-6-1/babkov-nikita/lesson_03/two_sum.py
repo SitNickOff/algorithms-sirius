c6058249bfcc663e7351fc0792f7b212d5bbfb83
RealniyChel2003 <babkov03@mail.ru>
2021-12-23 19:06:23 +0300

    added lesson_03

--- group-6-1/babkov-nikita/lesson_03/two_sum.py
+++ group-6-1/babkov-nikita/lesson_03/two_sum.py
@@ -7,14 +7,33 @@
 # с применеием сортировки или вспомогательных структур
 
 def two_sum_primary(numbers, sum):
-    return [1, 9]
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if j != i:
+                if numbers[i] + numbers[j] == sum:
+                    return [numbers[i], numbers[j]]
+    return -1
+
 
 def two_sum_optimized(numbers, sum):
-    return [3, 5]
+    for i in range(len(numbers)):
+        if (sum - numbers[i]) in numbers:
+            return [numbers[i], sum - numbers[i]]
+    return -1
+
+
+def test(res, aw_res):
+    if res == aw_res:
+        print("it's ok")
+    else:
+        print('not ok')
+
+
+test(two_sum_primary([1, 2, 3, 5, 9], 10), [1, 9])
+test(two_sum_optimized([1, 2, 3, 5, 9], 8), [3, 5])
 
 # numbers = list(map(int, input().split()))
 # sum = int(input())
 
 # print('primary', two_sum_primary(numbers, sum))
 # print('optimized', two_sum_primary(numbers, sum))
-
