bc6e8d4c789ed1b7224f7aaf6d49a514089e6c54
asqMyrrh <89976417+asqMyrrh@users.noreply.github.com>
2022-01-15 21:15:06 +0300

    Add files via upload

--- lesson_03/binary_search.py
+++ lesson_03/binary_search.py
@@ -4,4 +4,35 @@
 # Решить задачу применяя алгоритм бинарного поиска
 
 def binary_search(numbers, x):
-    return -1
+
+    left = -1
+    right = len(numbers)
+    if x in numbers:
+        while right > left +1:
+            middle = (left + right) // 2
+            if numbers[middle] >= x:
+                right = middle 
+            else:
+                left = middle 
+        return right
+    else:
+        return -1
+
+
+def test(result,awaiting_res):
+    if result == awaiting_res:
+        print("ok")
+    else:
+        print('not ok')
+
+
+a = [10,2,3,6,8,1]
+print('введите символ для поиска в списке')
+n = int(input())
+print(sorted(a))
+print(binary_search(sorted(a),n))
+a.sort()
+test(binary_search(a, 1), 0)
+test(binary_search(a, 2), 1)
+test(binary_search(a, 15), -1)
+test(binary_search(a, 5), -1)
--- lesson_03/example01.py
+++ lesson_03/example01.py
@@ -1,4 +1,3 @@
-  
 from math import pi
 
 def circle_area(radius):
