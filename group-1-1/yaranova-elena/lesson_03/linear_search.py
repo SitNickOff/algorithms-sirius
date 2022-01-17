fd1ca19c734fa57e37ef38f9a873d9cfd95bef90
MinLiway <89976190+MinLiway@users.noreply.github.com>
2022-01-17 19:03:19 +0300

    lesson3

--- group-1-1/yaranova-elena/lesson_03/linear_search.py
+++ group-1-1/yaranova-elena/lesson_03/linear_search.py
@@ -4,4 +4,11 @@
 # Решить задачу применяя алгоритм бинарного поиска
 
 def linear_search(numbers, x):
+    for i in range(len(numbers)):
+        if numbers[i] == x:
+            return i
     return -1
+
+numbers=[1,2,3,4,5,6]
+print(numbers)
+print(linear_search(numbers,5))
--- group-1-1/yaranova-elena/lesson_03/two_sum.py
+++ group-1-1/yaranova-elena/lesson_03/two_sum.py
@@ -3,14 +3,54 @@
 # Гарантируется, что такие элементы в массиве есть.
 
 # 1. Решить задачу применяя наивный алгоритм
-# 2. Предлжить второе решение, которое оптимизрует наивны алгоритм 
+# 2. Предлжить второе решение, которое оптимизрует наивный алгоритм 
 # с применеием сортировки или вспомогательных структур
 
-def two_sum_primary(numbers, sum):
-    return [1, 9]
+def two_sum(num, s):
+    for i in range(s):
+        a=s-i
+        if a in num and i in num:
+            print(a,i)
 
-def two_sum_optimized(numbers, sum):
-    return [3, 5]
+
+num=[1,2,3,4]
+s=3
+print(two_sum(num,s))
+
+
+def bin_search(num, x):
+    low=0
+    high=len(num) - 1
+
+    while low<=high:
+        middle = (low+high) //2
+
+        if num[middle]==x:
+            return x
+
+        elif num[middle]>x:
+            high = middle - 1
+
+        elif num[middle]<x:
+            low = middle + 1
+    return -1
+
+def two_sum_norm(num, sum):
+    num=sorted(num)
+    result=[]
+    print(num)
+    for i in num:
+        x=sum-i
+        if bin_search(num,x) != -1:
+            result.append([bin_search(num,x), i])
+
+    return str(result)
+
+
+
+num=[1,2,3,6,5]
+sum=7
+print('optimized', two_sum_norm(num,sum))
 
 # numbers = list(map(int, input().split()))
 # sum = int(input())
