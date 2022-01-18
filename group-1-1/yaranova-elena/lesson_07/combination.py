b2b4f480ed79a06099af604d2b0abd0ab935dfbd
MinLiway <89976190+MinLiway@users.noreply.github.com>
2022-01-18 18:46:46 +0300

    less7

--- group-1-1/yaranova-elena/lesson_07/combination.py
+++ group-1-1/yaranova-elena/lesson_07/combination.py
@@ -27,3 +27,37 @@
 
 # 92
 # wa wb wc xa xb xc ya yb yc za zb zc
+
+
+
+def letterCombinations(digits):
+    if len(digits) == 0:
+        return []
+    characters = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+    result = []
+    solve(digits, characters, result)
+    return result
+
+
+
+
+def solve(digits, characters, result, current_string="", current_level=0):
+    if current_level == len(digits):
+        result.append(current_string)
+        return
+    for i in characters[int(digits[current_level])]:
+        solve(digits, characters, result, current_string + i, current_level + 1)
+
+
+
+def test(result, expected):
+    if result != expected:
+        print(f'Ошибка: {result} != {expected}')
+    else:
+        print('Ok!')
+
+
+
+
+test(letterCombinations("23"), ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+test(letterCombinations("92"), ['wa', 'wb', 'wc', 'xa', 'xb', 'xc', 'ya', 'yb', 'yc', 'za', 'zb', 'zc'])
