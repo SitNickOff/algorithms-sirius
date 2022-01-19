380989f2b57a1636aa3d851f5d5746c7e8742279
Екатерина Зенкова <katyu2003@yandex.ru>
2022-01-19 16:04:41 +0300

    lesson-06

--- group-6-1/zenkova-ekaterina/lesson_06/bracket_seq.py
+++ group-6-1/zenkova-ekaterina/lesson_06/bracket_seq.py
@@ -22,11 +22,22 @@
 # Выведите «True» или «False».
 
 def is_correct_bracket_seq(string):
-    #  Your code
-    #  “ヽ(´▽｀)ノ”
-    pass
+    my_stack = []
+    open_list = ["[","{","("]
+    close_list = ["]","}",")"] 
+    if string[0] == ")" or string[0] == "]" or string[0] == "}":
+        return False
+    for i in string:
+        if i in open_list:
+            my_stack.append(i)
+        elif i in close_list:
+            if (open_list[close_list.index(i)] == my_stack[len(my_stack)-1]) and (len(my_stack) > 0):
+                my_stack.pop()
+            else:
+                return False
+    if len(my_stack) == 0:
+        return True
 
-# print(is_correct_bracket_seq(str(input())))
   
 def test(result, expected):
     if result != expected:
