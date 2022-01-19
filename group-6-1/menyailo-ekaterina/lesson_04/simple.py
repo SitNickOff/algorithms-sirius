41453156e6200a0c0e3812cf1d1df9fdbf7261c9
Katya Menyaylo <katya/menyaylo/04@gmail.com>
2022-01-19 15:51:20 +0300

    сделала 4ый урок

---
+++ group-6-1/menyailo-ekaterina/lesson_04/simple.py
@@ -0,0 +1,20 @@
+def simple(n):
+    k = 0 
+    for i in range(2,int(n**0.5)):
+        if n % i ==0:
+            return False
+    else:
+        return True
+
+def test(n, result):
+    if simple(n) != result:
+        print('Ошибка! Ожидали: ', result, ' -  Получили: ', simple(n))
+    else:
+        print('Отлично: ', result, '==', simple(n))
+
+
+test(3, True)
+test(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, False)
+test(5, True)
+test(16, False)
+test(3, True)
